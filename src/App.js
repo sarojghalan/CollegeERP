86a40c6bae9d358b3ed513e689a509c769df8cce
Hypekage <jmestmnag@gmail.com>
2023-02-01 22:34:35 +0545

    particle js created and login model

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Body from './Component/Body'
 import Header from './Component/Header'
+import Login from './Demo/Login'
 import HomeRoutes from './Routes/HomeRoutes'
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Body />
+      {/* <Header />
+      <Body /> */}
+      <Login />
     </>
   )
 }
---
+++ src/Demo/Login.js
@@ -0,0 +1,120 @@
+import React from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+const Login = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+  const LoginForm = () => (
+    <div className="login-card">
+    <h2>Welcome Back</h2>
+      <input type="text" placeholder="Enter Username Here..." className="login-input" />
+      <br />
+      <input type="password" placeholder="Enter Password Here..." className="login-input" />
+      <br />
+      <div className="btn-main">
+        <button type="submit">Login</button>
+      </div>
+    </div>
+  );
+  return (
+    <>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#0d47a1",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.8,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 4,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <LoginForm />
+      </div>
+    </>
+  );
+};
+
+export default Login;
